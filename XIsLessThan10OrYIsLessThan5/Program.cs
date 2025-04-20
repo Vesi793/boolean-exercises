bcb7f0b08a24eba06bd27871cb5a06117edd0985
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:47:07 +0100

    Add folder: XIsLessThan10OrYIsLessThan5

---
+++ XIsLessThan10OrYIsLessThan5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace XIsLessThan10OrYIsLessThan5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            bool result = x < 10 || y < 5;
+            Console.WriteLine(result);
+        }
+    }
+}
