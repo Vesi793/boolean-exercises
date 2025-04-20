9f0991b4bd1d1cfe86e2476808f1e5b814253ad4
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:43:28 +0100

    Add folder: CheckThe3Numbers

---
+++ CheckThe3Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckThe3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 18;
+            int c = 12;
+
+            bool result = (a > 20 && b <= 18) || (c > 10);
+
+            Console.WriteLine(result);
+        }
+    }
+}
