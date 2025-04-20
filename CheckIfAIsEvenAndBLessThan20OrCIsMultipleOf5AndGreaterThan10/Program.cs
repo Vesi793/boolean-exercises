ffd77e6a9e4b42ce8a2697a3070ca3dcaaa397f9
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:39:50 +0100

    Add folder: CheckIfAIsEvenAndBLessThan20OrCIsMultipleOf5AndGreaterThan10

---
+++ CheckIfAIsEvenAndBLessThan20OrCIsMultipleOf5AndGreaterThan10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CheckIfAIsEvenAndBLessThan20OrCIsMultipleOf5AndGreaterThan10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+            int b = 18;
+            int c = 25;
+
+            bool result = ((a % 2 == 0) && (b < 20))
+            || ((c % 5 == 0) && (c > 10));
+
+            Console.WriteLine(result);
+        }
+    }
+}
