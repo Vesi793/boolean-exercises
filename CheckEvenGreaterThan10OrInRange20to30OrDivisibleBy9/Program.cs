1e6a43335dc54b40c6a23d7a1c9951953a6b0e3f
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:38:56 +0100

    Add folder: CheckEvenGreaterThan10OrInRange20to30OrDivisibleBy9

---
+++ CheckEvenGreaterThan10OrInRange20to30OrDivisibleBy9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckEvenGreaterThan10OrInRange20to30OrDivisibleBy9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 28;
+            int c = 18;
+
+            bool result = ((a % 2 == 0) &&
+                (a > 10)) || ((b < 30) && (b > 20))
+            || (c % 9 == 0);
+
+            Console.WriteLine(result);
+        }
+    }
+}
