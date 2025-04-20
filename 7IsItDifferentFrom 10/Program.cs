23c4ab75b62a0ca81dc006ad9d8868ae84492126
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:34:43 +0100

    Add folder: 7IsItDifferentFrom 10

---
+++ 7IsItDifferentFrom 10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _7IsItDifferentFrom_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+
+            bool result = num1 != 10;
+
+            Console.WriteLine(result);
+        }
+    }
+}
