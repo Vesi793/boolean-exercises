10efdc5b22d299b4a4b20b2162787aec2048bde7
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:42:34 +0100

    Add folder: CheckIfXIsOddAndGreaterThanYOrZIsNotEqualToXPlusY

---
+++ CheckIfXIsOddAndGreaterThanYOrZIsNotEqualToXPlusY/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckIfXIsOddAndGreaterThanYOrZIsNotEqualToXPlusY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9;
+            int y = 4;
+            int z = 20;
+
+            bool result = ((x % 2 != 0) && (x > y))
+                || (z != (x = y));
+
+            Console.WriteLine(result);
+               
+        }
+    }
+}
