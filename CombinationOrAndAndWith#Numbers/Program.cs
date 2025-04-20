0bcf73d481f6f7a807ee24415240746d5bdc46a6
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:44:18 +0100

    Add folder: CombinationOrAndAndWith#Numbers

---
+++ CombinationOrAndAndWith#Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CombinationOrAndAndWith_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 25;
+            int c = 40;
+
+            bool result = (a > 20 && a < 50) ||
+                (b > 30 && b < 40) || (c == 40);
+
+            Console.WriteLine(result);
+        }
+    }
+}
