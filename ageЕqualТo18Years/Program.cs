546dd79b90dc87c2bc87d82f8e84683f59543afd
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:35:53 +0100

    Add folder: AgeЕqualТo18Years

---
+++ ageЕqualТo18Years/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ageЕqualТo18Years
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+
+            bool result = age == 18;
+
+            Console.WriteLine(result);
+        }
+    }
+}
