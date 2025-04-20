55ed9655ad382c776b2ecdcfba5e1a4f54d0b537
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:40:52 +0100

    Add folder: CheckIfSumIsEvenAndAIsNotEqualToBOrCIsNotInRange

---
+++ CheckIfSumIsEvenAndAIsNotEqualToBOrCIsNotInRange/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CheckIfSumIsEvenAndAIsNotEqualToBOrCIsNotInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 4;
+            int c = 7;
+            int d = -1;
+
+            bool range = (a > 10 && a < 20)
+            || (b % 2 == 0);
+
+            bool result = !(c % 2 == 0) || (d < 0);
+
+            Console.WriteLine(result);
+            
+
+
+        }
+
+    }
+}
