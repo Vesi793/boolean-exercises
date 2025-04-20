80c5d5f3674835e29c0cb4e31d84257f68279201
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:37:06 +0100

    Add folder: aIs moreThan10OrEqualAndBIsLessThanOrEqual10

---
+++ aIs moreThan10OrEqualAndBIsLessThanOrEqual10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace aIs_moreThan10OrEqualAndBIsLessThanOrEqual10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 8;
+
+            bool result = a >= 10 && b <= 10;
+
+            Console.WriteLine(result);
+        }
+    }
+}
