60acd617d0e0cc3ccb612ea1cbbf7100c0cd938c
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:45:36 +0100

    Add folder: IsТheNumberBigger

---
+++ IsТheNumberBigger/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace whetherTwoNumbersAreEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            bool result = num < 5;
+
+            Console.WriteLine(result);
+        }
+    }
+}
