78e4d2a504b8ecd662dae423c872160257053607
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-20 20:41:41 +0100

    Add folder: CheckIfXIsDivisibleBy3AndGreaterThan10OrYIsEvenAndNotEqualToX

---
+++ CheckIfXIsDivisibleBy3AndGreaterThan10OrYIsEvenAndNotEqualToX/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckIfXIsDivisibleBy3AndGreaterThan10OrYIsEvenAndNotEqualToX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            int y = 8;
+
+            bool result = ((x % 3 == 0) && (x > 10))
+                || ((y % 2 == 0) && (y != x));
+
+            Console.WriteLine(result);
+        }
+    }
+}
